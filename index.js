605d3c866d84b6134829b0939eef1ccfbd014849
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-10-01 11:09:16 +0200

    ..

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
+var mime = require('mime');
+
+var handleError = function(err, res) {
+  var filePath = extract('/error.html');
+  fs.readFile(filePath, function(err, data) {
+    res.end(data);
+  });
+};
+
 var server = http.createServer(function(req, res) {
   console.log('Responding to a request.');
-  res.end('<h1>Hello, World!!!!</h1>');
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      //Silver Challenge: Providing a MIME Type Dynamically
+      res.setHeader('Content-Type', mime.getType(filePath));
+      res.end(data);
+    }
+  });
 });
 server.listen(3000);
